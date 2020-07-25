ff902d36881587ffb98db26ec5d8820b310975c5
Crhistian Caraballo <cristianjcj35@gmail.com>
2020-07-24 21:47:40 -0500

    Query, sorting and pagination also CORS implemented

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,6 +29,15 @@ namespace API
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddAplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                }
+                );
+            }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +52,7 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 

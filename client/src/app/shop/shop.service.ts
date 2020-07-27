4421d8a9d8f960a1f9a777008b6efd89ab2e5d37
Crhistian Caraballo <cristianjcj35@gmail.com>
2020-07-27 12:41:39 -0500

    Routing implemented and view details

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IBrand } from '../shared/models/brand';
 import { IType } from '../shared/models/productType';
 import { delay, map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root',
@@ -34,6 +35,9 @@ export class ShopService {
         delay(1000),
         map((res) => res.body)
       );
+  }
+  getProduct(id: number) {
+    return this.http.get<IProduct>('/api/products/' + id);
   }
   getBrands() {
     return this.http.get<IBrand[]>('/api/products/brands');
